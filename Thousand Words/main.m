a419d9d85ea899cde3cb0ff635beaec650fbc139
Edu González <Edu@MacBook-Pro-de-Edu.local>
2015-02-12 13:02:43 +0100

    Initial Commit

---
+++ Thousand Words/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Thousand Words
+//
+//  Created by Edu González on 12/2/15.
+//  Copyright (c) 2015 Edu González. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
